f8d6e09fdd38be804df9d33f5dec97c165a3b0df
loubna saadi <loubnasaadi88@gmail.com>
2023-05-23 23:14:31 +0100

    r

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,13 @@ import 'package:jobfaster_application/screens/profile_screen.dart';
 import 'package:jobfaster_application/screens/companyinfo_screen.dart';
 import 'package:jobfaster_application/screens/companyprofile_screen.dart';
 import 'package:jobfaster_application/screens/joboffer_screen.dart';
-void main() async{
-   WidgetsFlutterBinding.ensureInitialized();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
   final auth = FirebaseAuth.instance;
   final user = auth.currentUser;
-  
 
   runApp(const MyApp());
 }
@@ -38,24 +38,28 @@ class MyApp extends StatelessWidget {
       // home: WelcomeScreen(),
       initialRoute: WelcomeScreen.screenRoutes,
       routes: {
-        WelcomeScreen.screenRoutes:(context)=>const WelcomeScreen(),
-       SignInScreen.screenRoutes:(context)=> const SignInScreen(),
-        RegistrationScreen.screenRoutes:(context)=>const RegistrationScreen(),
+        WelcomeScreen.screenRoutes: (context) => const WelcomeScreen(),
+        SignInScreen.screenRoutes: (context) => const SignInScreen(),
+        RegistrationScreen.screenRoutes: (context) =>
+            const RegistrationScreen(),
         Home.screenRoute: (context) => const Home(),
         IdentityScreen.screenRoute: (context) => const IdentityScreen(),
         EmployerScreen.screenRoute: (context) => const EmployerScreen(),
         TestScreen.screenRoute: (context) => const TestScreen(),
         ProfileScreen.screenRoute: (context) => const ProfileScreen(),
         CompanyInfoScreen.screenRoute: (context) => const CompanyInfoScreen(),
-        CompanyProfileScreen.screenRoute: (context) => const CompanyProfileScreen(),
+        CompanyProfileScreen.screenRoute: (context) =>
+            const CompanyProfileScreen(),
         JobOfferScreen.screenRoute: (context) => const JobOfferScreen(),
-     ApplyScreen.screenRoute: (context) => ApplyScreen(
-              jobOffer: ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>,
-              jobId: '', // Pass the jobId here
-              companyId: '', // Pass the companyId here
-            ),
-
-
+        ApplyScreen.screenRoute: (context) {
+          final jobOffer = ModalRoute.of(context)!.settings.arguments
+              as Map<String, dynamic>;
+          return ApplyScreen(
+            jobOffer: jobOffer,
+            jobId: jobOffer['jobId'] ?? '',
+            companyId: jobOffer['companyId'] ?? '',
+          );
+        },
       },
     );
   }
