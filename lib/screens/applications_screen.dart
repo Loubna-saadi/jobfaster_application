88c2cafe817895f074e9025315660f6c615997da
loubna saadi <loubnasaadi88@gmail.com>
2023-05-25 15:27:58 +0100

    tryin to fix the cv save

--- lib/screens/applications_screen.dart
+++ lib/screens/applications_screen.dart
@@ -1,12 +1,11 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:dio/dio.dart';
-
+import 'package:path/path.dart' as path;
 
 class ApplicationsScreen extends StatelessWidget {
   static const String screenRoute = 'applications_screen';
@@ -122,34 +121,33 @@ class ApplicationsScreen extends StatelessWidget {
   }
 
   // Method to download the CV file
-void downloadCV(String url) async {
-  Dio dio = Dio();
-  try {
-    // Get the application documents directory
-    Directory? appDocDir = await getApplicationDocumentsDirectory();
-    String? appDocPath = appDocDir?.path;
+  void downloadCV(String url) async {
+    Dio dio = Dio();
+    try {
+      // Get the downloads directory path using the path_provider package
+      Directory? downloadsDirectory = await getDownloadsDirectory();
+      String? downloadsPath = downloadsDirectory?.path;
 
-    // Extract the file name from the URL
-    String fileName = url.substring(url.lastIndexOf("/") + 1);
+      // Extract the file name from the URL
+      String fileName = path.basename(url);
 
-    // Download the file
-    Response response = await dio.get(
-      url,
-      options: Options(
-        responseType: ResponseType.bytes,
-        followRedirects: false,
-        receiveTimeout: Duration(seconds: 30),
-      ),
-    );
+      // Download the file
+      Response response = await dio.get(
+        url,
+        options: Options(
+          responseType: ResponseType.bytes,
+          followRedirects: false,
+          receiveTimeout: Duration(seconds: 30),
+        ),
+      );
 
-    // Save the file to the application documents directory
-    File file = File('$appDocPath/$fileName');
-    await file.writeAsBytes(response.data, flush: true);
+      // Save the file to the device's Downloads directory
+      File file = File('$downloadsPath/$fileName');
+      await file.writeAsBytes(response.data, flush: true);
 
-    print('File downloaded at: ${file.path}');
-  } catch (e) {
-    print('Error downloading file: $e');
+      print('File downloaded at: ${file.path}');
+    } catch (e) {
+      print('Error downloading file: $e');
+    }
   }
-}
-
 }
